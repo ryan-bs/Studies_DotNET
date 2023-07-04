1c7995050d95b847a3927ed243d643df15180f8f
Ryan Breda Santos <estagio.sst2@sesims.com.br>
2023-07-04 12:11:06 -0400

    aula 4

---
+++ WebAPIs/Models/Login.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPIs.Models
+{
+    public class Login
+    {
+        public string email { get; set; }
+
+        public string senha { get; set; }
+
+        public string cpf { get; set; }
+
+    }
+}
