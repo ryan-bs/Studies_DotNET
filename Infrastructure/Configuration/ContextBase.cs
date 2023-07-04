3e751d5f9be21b1f060b0be7a8c8e306e2ba45ad
Ryan Breda Santos <estagio.sst2@sesims.com.br>
2023-07-04 09:26:39 -0400

    Add project files.

---
+++ Infrastructure/Configuration/ContextBase.cs
@@ -0,0 +1,43 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Configuration
+{
+    internal class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options) { }
+
+        public DbSet<Message> Message { get; set; }
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObeterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+        public string ObeterStringConexao()
+        {
+            return "Data Source=SESI-SST-49584\\SQLEXPRESS02;Initial Catalog=API_DDD_2022;Integrated Security=True";
+            //"Data Source=SESI-SST-49584\\SQLEXPRESS02;Initial Catalog=API_DDD_2022;Pooling=true;Database=API_DDD_2022;User Id=postgres;Password=1931;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+        }
+    }
+}
---
+++ Infrastructure/Repository/Generics/RepositoryGenerics.cs
@@ -0,0 +1,83 @@
+﻿using Domain.Interfaces.Generics;
+using Infrastructure.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository.Generics
+{
+    public class RepositoryGenerics<T> : IGeneric<T>, IDisposable where T : class
+    {
+
+        private readonly DbContextOptions<ContextBase> _OptionsBuilder;
+
+        public RepositoryGenerics()
+        {
+            _OptionsBuilder = new DbContextOptions<ContextBase>();
+        }
+
+        public async Task Add(T Objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Delete(T Objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> GetEntityById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<T>> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Update(T Objeto)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Disposed https://docs.microsoft.com/pt-br/dotnet/standard/garbage-collection/implementing-dispose
+        // Flag: Has Dispose already been called?
+        bool disposed = false;
+        // Instantiate a SafeHandle instance.
+        SafeHandle handle = new SafeFileHandle(IntPtr.Zero, true);
+
+
+
+        // Public implementation of Dispose pattern callable by consumers.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+
+        // Protected implementation of Dispose pattern.
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                handle.Dispose();
+                // Free any other managed objects here.
+                //
+            }
+
+            disposed = true;
+        }
+        #endregion
+
+    }
+}
